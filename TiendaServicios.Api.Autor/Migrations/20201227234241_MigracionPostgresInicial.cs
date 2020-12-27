acaafb4ca9eff68d4e6fb0e38e874b9b2b14ec09
John alexander ubaque laguna <alexander80143@gmail.com>
2020-12-27 18:49:06 -0500

    Add project files.

---
+++ TiendaServicios.Api.Autor/Migrations/20201227234241_MigracionPostgresInicial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TiendaServicios.Api.Autor.Migrations
+{
+    public partial class MigracionPostgresInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autorlibro",
+                columns: table => new
+                {
+                    AutorLibroId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: true),
+                    Apellido = table.Column<string>(type: "text", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    AutorLibroGuid = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autorlibro", x => x.AutorLibroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GradoAcademico",
+                columns: table => new
+                {
+                    GradoAcademicoId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: true),
+                    CentroAcademico = table.Column<string>(type: "text", nullable: true),
+                    FechaGrado = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    AutorLibroId = table.Column<int>(type: "integer", nullable: false),
+                    GradoAcademicoGuid = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GradoAcademico", x => x.GradoAcademicoId);
+                    table.ForeignKey(
+                        name: "FK_GradoAcademico_Autorlibro_AutorLibroId",
+                        column: x => x.AutorLibroId,
+                        principalTable: "Autorlibro",
+                        principalColumn: "AutorLibroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GradoAcademico_AutorLibroId",
+                table: "GradoAcademico",
+                column: "AutorLibroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GradoAcademico");
+
+            migrationBuilder.DropTable(
+                name: "Autorlibro");
+        }
+    }
+}
